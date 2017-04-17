04a8560e4179e67b26b0e54fc57a18618571e055
xhute.lwk <xhute.lwk@xhutedeMacBook-Pro.local>
2017-04-17 18:20:07 +0800

    string format

---
+++ ex5.py
@@ -0,0 +1,5 @@
+my_name = "xhute"
+my_age = 34
+print "%s is %d years old." % (my_name , my_age)
+
+# Doc for Python String Formatting : https://docs.python.org/3/library/stdtypes.html#printf-style-string-formatting
